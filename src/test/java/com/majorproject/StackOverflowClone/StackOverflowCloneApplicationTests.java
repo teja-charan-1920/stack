f6543df6f2baba6033137cda9a4671ed92ac874f
Surendra Chakravarthula <surendrachakravarthula666@gmail.com>
2023-06-17 10:32:39 +0530

    Add project directory

---
+++ src/test/java/com/majorproject/StackOverflowClone/StackOverflowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorproject.StackOverflowClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverflowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
