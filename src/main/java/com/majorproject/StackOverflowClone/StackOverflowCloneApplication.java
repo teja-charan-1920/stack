3a5a6d981022520a6ef1873ffd50e1860753c289
Surendra Chakravarthula <surendrachakravarthula666@gmail.com>
2023-06-20 09:49:12 +0530

    Refactor

--- src/main/java/com/majorproject/StackOverflowClone/StackOverflowCloneApplication.java
+++ src/main/java/com/majorproject/StackOverflowClone/StackOverflowCloneApplication.java
@@ -1,9 +1,5 @@
 package com.majorproject.StackOverflowClone;
 
-import com.majorproject.StackOverflowClone.model.User;
-import com.majorproject.StackOverflowClone.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,5 +8,4 @@ public class StackOverflowCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StackOverflowCloneApplication.class, args);
 	}
-
 }
--- src/main/java/com/majorproject/StackOverflowClone/controller/QuestionController.java
+++ src/main/java/com/majorproject/StackOverflowClone/controller/QuestionController.java
@@ -57,6 +57,7 @@ public class QuestionController {
         questionService.updateQuestion(question);
         return "redirect:/viewQuestion?questionId=" + questionId;
     }
+
         @PostMapping("/questions/ask")
         public String addQuestion(@ModelAttribute Question question, @RequestParam("tag") String tags) {
             questionService.addQuestion(question, tags);
