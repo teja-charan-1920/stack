36c5dd25a874b7045cc76502b52edfb1a65d97e6
Surendra Chakravarthula <surendrachakravarthula666@gmail.com>
2023-06-20 11:40:46 +0530

    Add dto

--- src/main/java/com/majorproject/StackOverflowClone/service/AnswerService.java
+++ src/main/java/com/majorproject/StackOverflowClone/service/AnswerService.java
@@ -3,7 +3,10 @@ package com.majorproject.StackOverflowClone.service;
 import com.majorproject.StackOverflowClone.model.Answer;
 import com.majorproject.StackOverflowClone.repository.AnswerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnswerService {
@@ -15,5 +18,9 @@ public class AnswerService {
 
     public void updateAnswer(Answer answer){
         answerRepository.save(answer);
+    }
+
+    public List<Answer> fetchAnswers(String sortBy) {
+      return answerRepository.findAll(Sort.by(sortBy));
     }
 }
